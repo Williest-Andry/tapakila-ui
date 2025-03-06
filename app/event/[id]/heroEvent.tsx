c8a15bb6030d0b6a60a4746dca65e8cdfbf35874
Williest <williestnyainaandry@gmail.com>
2025-03-06 20:30:32 +0300

    fix: divide event/[id] page to heroEvent, ticketsTable and similarEvents components

--- app/event/[id]/heroEvent.tsx
+++ app/event/[id]/heroEvent.tsx
@@ -8,7 +8,7 @@ export default function HeroEvent() {
             <Flex gap="25vh" direction="column" width="50%">
                 <Stack gap="6">
                     <Heading fontWeight="bold" size="5xl">Title</Heading>
-                    <Heading fontWeight="normal" size="lg">Categories</Heading>
+                    <Heading fontWeight="normal" size="lg" border="2px solid" borderRadius="md" borderColor="yellow.400" w="7vw">Categories</Heading>
                     <Heading fontWeight="medium" size="xl">Place</Heading>
                     <Heading fontWeight="medium" size="xl">Date</Heading>
                     <Heading fontWeight="medium" size="xl">Organizer</Heading>
--- app/event/[id]/page.tsx
+++ app/event/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { AbsoluteCenter, Box, Button, Center, Container, Flex, Heading, Image, Stack, Table, Wrap } from "@chakra-ui/react";
+import { Button, Center, Container} from "@chakra-ui/react";
 import HeroEvent from "./heroEvent";
 import TicketsTable from "./ticketsTable";
 import SimilarEvents from "./similarEvents";
@@ -6,18 +6,27 @@ import SimilarEvents from "./similarEvents";
 export default async function Event({ params }) {
     const tickets: string[] = ["silver", "gold", "vip"];
     const similarEvents: string[] = ["silver", "gold", "vip"];
+    const categories: string[] = ["Live"];
 
     return (
         <Container>
             <HeroEvent />
 
+            <br></br>
+            <br></br>
+
             <TicketsTable tickets={tickets}/>
+
+            <br></br>
+            <br></br>
 
             <Center>
                 <Button colorPalette="blue" variant="outline" size="lg" w="10vw">
                     <a href="#">Réserver</a>
                 </Button>
             </Center>
+
+            <br></br>
 
             <SimilarEvents similarEvent={similarEvents}/>
 
