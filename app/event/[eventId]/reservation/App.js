703748d7ab6dcbbdb65f57cafc305feb6e4d437d
Kef <hei.kephas@gmail.com>
2025-03-20 16:16:16 +0300

    fix: move all about reservation to app/event/[eventId]/reservation

---
+++ app/event/[eventId]/reservation/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TicketReservation from './TicketReservation';
+
+const App = () => {
+    return (
+        <div className="App">
+            <TicketReservation />
+        </div>
+    );
+};
+
+export default App;
---
+++ app/event/[eventId]/reservation/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TicketReservation from './TicketReservation';
+
+const Page = () => {
+  return (
+    <div className="App">
+      <TicketReservation />
+    </div>
+  );
+};
+
+export default Page;
