4c3a3f0194339a9f880d41992a4f91e6d05af1ff
Kef <hei.kephas@gmail.com>
2025-03-23 17:41:31 +0300

    feat: app/event/[eventId]/reservation

---
+++ app/event/[eventId]/reservation/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Ticket from "../../../../../../Back-end/api/entity/Ticket.js"
+
+export default function ReservationPage() {
+  return (
+    <div>On reservation page</div>
+  )
+}
