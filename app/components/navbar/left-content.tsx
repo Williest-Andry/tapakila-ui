25ef5553ded3ae2cec558cddd9ae9c4982cecd81
Kef <hei.kephas@gmail.com>
2025-04-03 13:06:40 +0300

    pull

--- app/components/navbar/left-content.tsx
+++ app/components/navbar/left-content.tsx
@@ -1,10 +1,11 @@
-import { Button, Flex, Heading, Link } from "@chakra-ui/react";
-import { GoTriangleDown } from "react-icons/go";
+import { Flex, Link, Image } from "@chakra-ui/react";
 
 export default function LeftNavbarContent(){
     return (
         <Flex alignItems="center" gap={2}>
-            <Link href="/" _hover={{textDecoration: "none"}}><Heading size="5xl" >Logo</Heading></Link>
+            <Link href="/" _hover={{textDecoration: "none"}}>
+                <Image src="/TapakilaLogo.svg" alt="Tapakila"/>
+            </Link>
         </Flex>
     );
 }
