27fee055f3ec966e0533543db9b65f7c1b596a72
Williest-Andry <williestnyainaandry@gmail.com>
2026-05-28 16:16:45 +0300

    feat: add api model types

---
+++ types/api.types.ts
@@ -0,0 +1,27 @@
+import type { paths } from "@/lib/api/schema.d";
+
+export type Event =
+  paths["/events"]["get"]["responses"][200]["content"]["application/json"]["data"][number];
+
+export type User =
+  paths["/auth/me"]["get"]["responses"][200]["content"]["application/json"];
+
+export type TicketType =
+  paths["/events/{eventId}/ticket-types"]["get"]["responses"][200]["content"]["application/json"][number];
+
+export type Booking =
+  paths["/bookings"]["get"]["responses"][200]["content"]["application/json"]["data"][number];
+
+export type Category =
+  paths["/event-categories"]["get"]["responses"][200]["content"]["application/json"]["data"][number];
+
+export type AuthTokens =
+  paths["/auth/login"]["post"]["responses"][200]["content"]["application/json"];
+
+export type PaginatedMeta =
+  paths["/events"]["get"]["responses"][200]["content"]["application/json"]["meta"];
+
+export type PaginatedResponse<T> = {
+  data: T[];
+  meta: PaginatedMeta;
+};
